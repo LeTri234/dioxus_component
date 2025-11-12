5236c55d3ddb3671373b74cf4ba4391da50d40dd
letri <>
2025-11-12 10:56:42 +0700

    add badge component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,11 +1,13 @@
 pub mod accordion;
 pub mod avatar;
+pub mod badge;
 pub mod button;
 pub mod spinner;
 pub mod tooltip;
 
 pub use accordion::*;
 pub use avatar::*;
+pub use badge::*;
 pub use button::*;
 pub use spinner::*;
 pub use tooltip::*;
